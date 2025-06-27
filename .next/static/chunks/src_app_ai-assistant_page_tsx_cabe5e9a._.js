9912831e38300f4daf3b7723c8e2d52a4d2f7a36
jamshaidAfzal23 <167150964+jamshaidAfzal23@users.noreply.github.com>
2025-06-27 20:47:55 +1000

    Add .next/static/chunks/src_app_ai-assistant_page_tsx_cabe5e9a._.js 1751021298882

---
+++ .next/static/chunks/src_app_ai-assistant_page_tsx_cabe5e9a._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_649d5633._.js"
+],
+    source: "dynamic"
+});
